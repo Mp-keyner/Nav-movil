4e89c20d0c9a92da744d22afd87b9d112484e09f
Mp-keyner <keyneroswaldo0@gmail.com>
2023-05-05 22:58:00 -0500

    5 de mayo del 2023

---
+++ index.js
@@ -0,0 +1,10 @@
+const section = document.querySelector('section[id]')
+
+function scrolActive() {
+    const scrollY = window.pageYOffset
+
+    section.forEach((current) => {
+        const sectionheight = current.scrollHeight
+
+    })
+}
